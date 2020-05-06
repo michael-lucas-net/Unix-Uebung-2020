3b0fad6e1bc7bf735ec4bb2775e09b17ae7f3d41
Michael Lucas <michiqt@live.de>
2020-05-06 20:12:07 +0200

    Aufgabe 1 Skript

---
+++ Aufgabe1/loesung/lister.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Michael Lucas - Gruppe 17
+
+# Ordner erstellen -p = Ignorieren, falls bereits vorhanden
+mkdir -p info
+
+# Alle Textdateien in mytextfiles schreiben.
+# 2> /dev/null = Fehlermeldung verstecken
+ls *.txt 2> /dev/null > ./info/mytextfiles.txt
+
+
+
