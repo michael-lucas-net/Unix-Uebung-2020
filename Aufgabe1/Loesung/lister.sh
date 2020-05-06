92853bd386afed98d2bb051d41814ebac87cb002
Michael Lucas <michiqt@live.de>
2020-05-06 22:05:50 +0200

    Untypische Namen einschließen

--- Aufgabe1/Loesung/lister.sh
+++ Aufgabe1/Loesung/lister.sh
@@ -6,4 +6,5 @@ mkdir -p info
 
 # Alle Textdateien in mytextfiles schreiben.
 # 2> /dev/null = Fehlermeldung verstecken
-ls *.txt 2> /dev/null > ./info/mytextfiles.txt
+# -- vor dem *, um auch untypische Dateinamen einzubeziehen (z.B. "-f datei.txt") 
+ls -- *.txt 2> /dev/null > ./info/mytextfiles.txt
