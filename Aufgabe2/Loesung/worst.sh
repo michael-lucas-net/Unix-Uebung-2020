f7fc4228d8fa84c9c19886a0acb2b65d5fa0c5c0
Michael Lucas <michiqt@live.de>
2020-05-21 18:03:17 +0200

    Numerische Vergleiche

--- Aufgabe2/Loesung/worst.sh
+++ Aufgabe2/Loesung/worst.sh
@@ -9,6 +9,7 @@
     # - k2 = Sortiert nach 2 tem Key (a, > b <, c)
     # - k1 sortiert zudem nach erstem ersten Key (in dem Fall Kennzahl) NACHDEM die Bewertung sortiert wurde
     # - r Absteigend
+    # - n numerisch sortieren (beruecksichtigt z.B. 10 > 9)
 # tee: Ausgabe in Console und Datei
 
 # Fuer STDOUT
@@ -16,4 +17,4 @@
     # cut: Schneidet Text ab
     # - d: teilt Text anhand von ',' auf
     # - f3 letzte Stelle(a, b, > c <)
-grep ",*," | sort -t ',' -k2 -k1 -r | tee "$1" | tail -n1 | cut -d "," -f3
+grep ",*," | sort -t ',' -k2 -k1 -r -n | tee "$1" | tail -n1 | cut -d "," -f3
