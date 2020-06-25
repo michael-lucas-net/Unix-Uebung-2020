3d8b18255eb8143b7733d43e480e914bb73c398f
Michael Lucas <michiqt@live.de>
2020-06-26 01:37:40 +0200

    Unnötige Dateien entfern

--- Aufgabe4/doodle.sh
+++
@@ -1,8 +0,0 @@
-cat tests/cache/kalender.html \
-	| sed -n "/<tr>/,/<\/tr>/p" \
-	| sed -n '6~7p' \
-	| grep "[AB][1-9] (" \
-	| grep -o "<div class=\"date\">.*<\/div>" \
-	| sed "s/<div class=\"date\">//g; s/<\/div>//g;" \
-	| sed "s/.*/|- &/g" \
-	> test.html
